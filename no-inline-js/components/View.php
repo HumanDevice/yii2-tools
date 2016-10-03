3362e225367e08fa2bdfbc5362d8b3c879b83f49
Bizley <pawel@positive.codes>
2016-10-03 12:22:52 +0200

    hash added

--- no-inline-js/components/View.php
+++ no-inline-js/components/View.php
@@ -3,6 +3,7 @@
 namespace common\components\web;
 
 use ArrayAccess;
+use Yii;
 use yii\di\Instance;
 use yii\helpers\Html;
 use yii\web\View as YiiView;
@@ -59,7 +60,7 @@ class View extends YiiView
         }
         if (!empty($this->js[self::POS_HEAD])) {
             $this->storage->offsetSet(self::STORAGE_JS_HEAD, $this->js[self::POS_HEAD]);
-            $lines[] = Html::jsFile(['js/head']);
+            $lines[] = Html::jsFile(['js/head', 'hash' => $this->hashKeys($this->js[self::POS_HEAD])]);
         }
 
         return empty($lines) ? '' : implode("\n", $lines);
@@ -78,7 +79,7 @@ class View extends YiiView
         }
         if (!empty($this->js[self::POS_BEGIN])) {
             $this->storage->offsetSet(self::STORAGE_JS_BEGIN, $this->js[self::POS_BEGIN]);
-            $lines[] = Html::jsFile(['js/begin']);
+            $lines[] = Html::jsFile(['js/begin', 'hash' => $this->hashKeys($this->js[self::POS_BEGIN])]);
         }
 
         return empty($lines) ? '' : implode("\n", $lines);
@@ -103,33 +104,47 @@ class View extends YiiView
         if ($ajaxMode) {
             $scripts = [];
             if (!empty($this->js[self::POS_END])) {
-                $scripts[] = implode("\n", $this->js[self::POS_END]);
+                $scripts = array_merge($scripts, $this->js[self::POS_END]);
             }
             if (!empty($this->js[self::POS_READY])) {
-                $scripts[] = implode("\n", $this->js[self::POS_READY]);
+                $scripts = array_merge($scripts, $this->js[self::POS_READY]);
             }
             if (!empty($this->js[self::POS_LOAD])) {
-                $scripts[] = implode("\n", $this->js[self::POS_LOAD]);
+                $scripts = array_merge($scripts, $this->js[self::POS_LOAD]);
             }
             if (!empty($scripts)) {
                 $this->storage->offsetSet(self::STORAGE_JS_AJAX, $scripts);
-                $lines[] = Html::jsFile(['js/ajax']);
+                $lines[] = Html::jsFile(['js/ajax', 'hash' => $this->hashKeys($scripts)]);
             }
         } else {
             if (!empty($this->js[self::POS_END])) {
                 $this->storage->offsetSet(self::STORAGE_JS_END, $this->js[self::POS_END]);
-                $lines[] = Html::jsFile(['js/end']);
+                $lines[] = Html::jsFile(['js/end', 'hash' => $this->hashKeys($this->js[self::POS_END])]);
             }
             if (!empty($this->js[self::POS_READY])) {
                 $this->storage->offsetSet(self::STORAGE_JS_READY, $this->js[self::POS_READY]);
-                $lines[] = Html::jsFile(['js/ready']);
+                $lines[] = Html::jsFile(['js/ready', 'hash' => $this->hashKeys($this->js[self::POS_READY])]);
             }
             if (!empty($this->js[self::POS_LOAD])) {
                 $this->storage->offsetSet(self::STORAGE_JS_LOAD, $this->js[self::POS_LOAD]);
-                $lines[] = Html::jsFile(['js/load']);
+                $lines[] = Html::jsFile(['js/load', 'hash' => $this->hashKeys($this->js[self::POS_LOAD])]);
             }
         }
 
         return empty($lines) ? '' : implode("\n", $lines);
+    }
+    
+    /**
+     * Generates hash based on the JS keys.
+     * @param array $js
+     * @return string
+     */
+    public function hashKeys($js)
+    {
+        $keys = '';
+        foreach (array_keys($js) as $key) {
+            $keys .= $key;
+        }
+        return sprintf('%x', crc32($keys . Yii::getVersion()));
     }
 }
